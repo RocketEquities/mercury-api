7c13109cfd05708e0ff70b61f79d76839829e57e
zander <zand3rs@gmail.com>
2017-06-27 12:36:11 +0800

    backward compatibility issue for mysql timestamp

--- db/schema/signups.sql
+++ db/schema/signups.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS signups;
 CREATE TABLE IF NOT EXISTS signups (
   id          bigint(11) NOT NULL AUTO_INCREMENT,
   email       varchar(256) NOT NULL,
-  created_at  timestamp NOT NULL DEFAULT now(),
+  created_at  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   updated_at  timestamp NOT NULL DEFAULT now(),
   PRIMARY KEY (`id`),
   UNIQUE KEY (`email`)
